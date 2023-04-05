e51115f63c10c2fb8962b3609fc798c9e85032a3
isaacwrk <isaacsouzawrk@gmail.com>
2023-04-04 22:14:49 -0300

    feat: creating authenticator token

---
+++ app/controllers/concerns/authenticable.rb
@@ -0,0 +1,15 @@
+module Authenticable
+private 
+    def authenticate_with_token
+        @token ||= request.headers['Authorization']
+
+        unless valid_token?
+            render json: { errors: 'Please provide any token in header Authorization '},
+            status: :unauthorized
+        end
+    end
+
+    def valid_token?
+        @token.present?
+    end
+end
