d2b0bc24e3d02eeecd63d411831c6b37ed21aa01
isaacwrk <isaacsouzawrk@gmail.com>
2023-04-04 21:27:21 -0300

    feat: adding search by params

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -3,5 +3,6 @@ end
 class Hero < ApplicationRecord
     validates :name, presence: true
 
+    scope :search, -> (term) { where('LOWER(name) LIKE ?', "%#{term.downcase}%") if term.present? }
     scope :sorted_by_name, -> { order(:name) }
 end
